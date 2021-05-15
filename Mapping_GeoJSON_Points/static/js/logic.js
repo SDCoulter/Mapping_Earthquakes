c9ca7c0b5fdb279b0231c23415132c51a956edf5
Shaun Coulter <shaundcoulter@gmail.com>
2021-05-15 16:29:50 -0400

    popup marker with airport, city, country info on night navigation map

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -2,10 +2,10 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([37.619,-122.375], 10);
+let map = L.map('mapid').setView([37.619,-122.375], 12);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -37,10 +37,11 @@ let sanFranAirport =
 ]};
 
 // Grabbing our GeoJSON data.
-//L.geoJSON(sanFranAirport).addTo(map);
 L.geoJSON(sanFranAirport, {
   pointToLayer: function(feature, latlng) {
     console.log(feature);
-    return L.marker(latlng);
+    return L.marker(latlng)
+    .bindPopup("<h2>" + feature.properties.name + "</h2> <hr /> \
+        <h3>" + feature.properties.city + ", " + feature.properties.country + "</h3>")
   }
 }).addTo(map);
